86b5fbb0f5288247dfb3a18221b3f226ab25f72f
davinngo <davinngo00@gmail.com>
2022-10-27 09:23:12 -0500

    clean up code

--- backend/rest_api.py
+++ backend/rest_api.py
@@ -61,18 +61,22 @@ def get_customer():
 # using POST as the method type
 @app.route('/api/customer/add', methods=['POST'])
 def add_customer():
+
     # using the get_json function to request data
     request_data = request.get_json()
+
     CountryID =             request_data['CountryID']
     CustomerFirstName =     request_data['CustomerFirstName']
     CustomerLastName =      request_data['CustomerLastName']
     CustomerAddress =       request_data['CustomerAddress']
     CustomerPhoneNumber =   request_data['CustomerPhoneNumber']
     CustomerEmail =         request_data['CustomerEmail']
-    conn = create_connection("database-2.cg9pywfjykka.us-east-2.rds.amazonaws.com", "admin", "FutureTechnologySolutionsGroup9", "KPJDB")
+
     # this sql query was created to add the new customer
     sql = "INSERT INTO Customer(CountryID, CustomerFirstName, CustomerLastName, CustomerAddress, CustomerPhoneNumber, CustomerEmail) VALUES ('%s', '%s', '%s', '%s', '%s', '%s')" % (CountryID, CustomerFirstName, CustomerLastName, CustomerAddress, CustomerPhoneNumber, CustomerEmail)
-    execute_query(conn, sql)
+    
+    execute_query(connection, sql)
+
     return 'POST successful'
 
 
